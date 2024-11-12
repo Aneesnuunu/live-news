5942e702979b65cd0d2dc3a1986a93743abfbda3
anees <aneesnuunu@gmail.com>
2024-11-12 20:29:04 +0530

    first

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -43,31 +43,47 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Wrap(
                 spacing: 10.0,
                 children: [
-                  'All', 'Business', 'Entertainment', 'Health', 'Sports', "Technology", 'Science', 'Politics',
+                  'All',
+                  'Business',
+                  'Entertainment',
+                  'Health',
+                  'Sports',
+                  "Technology",
+                  'Science',
+                  'Politics',
                 ]
                     .map((category) => ChoiceChip(
-                  label: Text(category),
-                  selected: newsProvider.currentCategory == category.toLowerCase() || (category == 'All' && newsProvider.currentCategory == ''),
-                  onSelected: (selected) {
-                    if (category == 'All') {
-                      newsProvider.fetchArticles('');
-                    } else {
-                      newsProvider.fetchArticles(category.toLowerCase());
-                    }
-                    newsProvider.setCurrentCategory(category == 'All' ? '' : category.toLowerCase());
-                  },
-                  selectedColor: Colors.pink[400],
-                  backgroundColor: Colors.grey[300],
-                  labelStyle: TextStyle(
-                    color: newsProvider.currentCategory == category.toLowerCase() || (category == 'All' && newsProvider.currentCategory == '')
-                        ? Colors.white
-                        : Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ))
+                          label: Text(category),
+                          selected: newsProvider.currentCategory ==
+                                  category.toLowerCase() ||
+                              (category == 'All' &&
+                                  newsProvider.currentCategory == ''),
+                          onSelected: (selected) {
+                            if (category == 'All') {
+                              newsProvider.fetchArticles('');
+                            } else {
+                              newsProvider
+                                  .fetchArticles(category.toLowerCase());
+                            }
+                            newsProvider.setCurrentCategory(category == 'All'
+                                ? ''
+                                : category.toLowerCase());
+                          },
+                          selectedColor: Colors.pink[400],
+                          backgroundColor: Colors.grey[300],
+                          labelStyle: TextStyle(
+                            color: newsProvider.currentCategory ==
+                                        category.toLowerCase() ||
+                                    (category == 'All' &&
+                                        newsProvider.currentCategory == '')
+                                ? Colors.white
+                                : Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ))
                     .toList(),
               ),
             ),
@@ -76,81 +92,85 @@ class _HomeScreenState extends State<HomeScreen> {
             child: newsProvider.isLoading
                 ? Center(child: CircularProgressIndicator())
                 : newsProvider.articles.isEmpty
-                ? Center(child: Text('No articles found.', style: TextStyle(fontSize: 18, color: Colors.black54)))
-                : ListView.builder(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              itemCount: newsProvider.articles.length,
-              itemBuilder: (context, index) {
-                final article = newsProvider.articles[index];
+                    ? Center(
+                        child: Text('No articles found.',
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.black54)))
+                    : ListView.builder(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        itemCount: newsProvider.articles.length,
+                        itemBuilder: (context, index) {
+                          final article = newsProvider.articles[index];
 
-                if (article.title.isEmpty ||
-                    article.description.isEmpty ||
-                    article.urlToImage.isEmpty) {
-                  return SizedBox.shrink();
-                }
+                          if (article.title.isEmpty ||
+                              article.description.isEmpty ||
+                              article.urlToImage.isEmpty) {
+                            return SizedBox.shrink();
+                          }
 
-                return Card(
-                  elevation: 4,
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  color: Colors.white,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(15),
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.network(
-                        article.urlToImage,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Image.asset(
-                            'assets/placeholder.png',
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
+                          return Card(
+                            elevation: 4,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            color: Colors.white,
+                            child: ListTile(
+                              contentPadding: EdgeInsets.all(15),
+                              leading: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(
+                                  article.urlToImage,
+                                  width: 100,
+                                  height: 100,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Image.asset(
+                                      'assets/placeholder.png',
+                                      width: 100,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    );
+                                  },
+                                ),
+                              ),
+                              title: Text(
+                                article.title,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF212121),
+                                ),
+                              ),
+                              subtitle: Text(
+                                article.description,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color(0xFF757575),
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NewsDetailScreen(
+                                      title: article.title,
+                                      description: article.description,
+                                      imageUrl: article.urlToImage,
+                                      url: article.url,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
                           );
                         },
                       ),
-                    ),
-                    title: Text(
-                      article.title,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF212121),
-                      ),
-                    ),
-                    subtitle: Text(
-                      article.description,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Color(0xFF757575),
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => NewsDetailScreen(
-                            title: article.title,
-                            description: article.description,
-                            imageUrl: article.urlToImage,
-                            url: article.url,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
           ),
         ],
       ),
